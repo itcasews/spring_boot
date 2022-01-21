f68fb68d281c0126af1677f098851c71d50c0d9d
itcasews <361610793@qq.com>
2022-01-21 15:39:23 +0800

    111

---
+++ src/main/java/com/example/spring_boot/service/UseService.java
@@ -0,0 +1,8 @@
+package com.example.spring_boot.service;
+
+import com.example.spring_boot.pojo.User;
+
+public interface UseService {
+    void insertMessage();
+    User selectMessgae();
+}
---
+++ src/main/java/com/example/spring_boot/service/UseServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.spring_boot.service;
+
+import com.example.spring_boot.pojo.User;
+
+public class UseServiceImpl implements UseService{
+    @Override
+    public void insertMessage() {
+
+    }
+
+    @Override
+    public User selectMessgae() {
+        return null;
+    }
+}
